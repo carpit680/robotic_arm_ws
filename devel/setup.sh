b812b45290fe2d4f0e27935628d5206ae855f40a
carpit680 <carpit680@gmail.com>
2020-04-18 11:50:15 +0530

    Initial commit

---
+++ devel/setup.sh
@@ -0,0 +1,1 @@
+/home/arpit/manas/ros_workspaces/robotic_arm_ws/devel/.private/catkin_tools_prebuild/setup.sh
---
+++ devel/share/robotic_arm_description/cmake/robotic_arm_descriptionConfig.cmake
@@ -0,0 +1,1 @@
+/home/arpit/manas/ros_workspaces/robotic_arm_ws/devel/.private/robotic_arm_description/share/robotic_arm_description/cmake/robotic_arm_descriptionConfig.cmake
